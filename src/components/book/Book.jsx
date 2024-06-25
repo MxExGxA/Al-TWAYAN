05923969510f9ecb040abee9b826840254470452
unknown <elsayedelghazy20@gmail.com>
2024-06-25 03:07:38 +0300

    second commit

--- src/components/book/Book.jsx
+++ src/components/book/Book.jsx
@@ -32,17 +32,14 @@ import {
 import pageFlip from "../../assets/sounds/pageFlip.mp3";
 import { MdNavigateNext, MdNavigateBefore } from "react-icons/md";
 
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 
 const Book = () => {
-  const [translateVal, setTranslateVal] = useState("300px");
   const [currentPage, setCurrentPage] = useState(26);
   const book = useRef();
 
   const handleOnFlip = (e) => {
-    console.log(e.data);
     setCurrentPage(e.data);
-    e.data !== 26 ? setTranslateVal("0") : setTranslateVal("300px");
     const audio = new Audio(pageFlip);
     audio.play();
   };
@@ -56,49 +53,51 @@ const Book = () => {
   };
 
   return (
-    <div className="book-container">
-      <HTMLFlipBook
-        ref={book}
-        className="book"
-        width={600}
-        height={650}
-        usePortrait={true}
-        size="stretch"
-        minHeight={200}
-        maxHeight={650}
-        minWidth={200}
-        maxWidth={600}
-        startPage={26}
-        onFlip={handleOnFlip}
-      >
-        <img src={i26} className="page" alt="" />
-        <img src={i25} className="page" alt="" />
-        <img src={i24} className="page" alt="" />
-        <img src={i23} className="page" alt="" />
-        <img src={i22} className="page" alt="" />
-        <img src={i21} className="page" alt="" />
-        <img src={i20} className="page" alt="" />
-        <img src={i19} className="page" alt="" />
-        <img src={i18} className="page" alt="" />
-        <img src={i17} className="page" alt="" />
-        <img src={i16} className="page" alt="" />
-        <img src={i15} className="page" alt="" />
-        <img src={i14} className="page" alt="" />
-        <img src={i13} className="page" alt="" />
-        <img src={i12} className="page" alt="" />
-        <img src={i11} className="page" alt="" />
-        <img src={i10} className="page" alt="" />
-        <img src={i9} className="page" alt="" />
-        <img src={i8} className="page" alt="" />
-        <img src={i7} className="page" alt="" />
-        <img src={i6} className="page" alt="" />
-        <img src={i5} className="page" alt="" />
-        <img src={i4} className="page" alt="" />
-        <img src={i3} className="page" alt="" />
-        <img src={i2} className="page" alt="" />
-        <img src={i1} className="page" alt="" />
-        <img src={cover} className="page" alt="" />
-      </HTMLFlipBook>
+    <>
+      <div className="book-container">
+        <HTMLFlipBook
+          ref={book}
+          className="book"
+          width={600}
+          height={650}
+          usePortrait={true}
+          size="stretch"
+          minHeight={350}
+          maxHeight={650}
+          minWidth={300}
+          maxWidth={600}
+          startPage={26}
+          onFlip={handleOnFlip}
+        >
+          <img src={i26} className="page" alt="" />
+          <img src={i25} className="page" alt="" />
+          <img src={i24} className="page" alt="" />
+          <img src={i23} className="page" alt="" />
+          <img src={i22} className="page" alt="" />
+          <img src={i21} className="page" alt="" />
+          <img src={i20} className="page" alt="" />
+          <img src={i19} className="page" alt="" />
+          <img src={i18} className="page" alt="" />
+          <img src={i17} className="page" alt="" />
+          <img src={i16} className="page" alt="" />
+          <img src={i15} className="page" alt="" />
+          <img src={i14} className="page" alt="" />
+          <img src={i13} className="page" alt="" />
+          <img src={i12} className="page" alt="" />
+          <img src={i11} className="page" alt="" />
+          <img src={i10} className="page" alt="" />
+          <img src={i9} className="page" alt="" />
+          <img src={i8} className="page" alt="" />
+          <img src={i7} className="page" alt="" />
+          <img src={i6} className="page" alt="" />
+          <img src={i5} className="page" alt="" />
+          <img src={i4} className="page" alt="" />
+          <img src={i3} className="page" alt="" />
+          <img src={i2} className="page" alt="" />
+          <img src={i1} className="page" alt="" />
+          <img src={cover} className="page" alt="" />
+        </HTMLFlipBook>
+      </div>
       <div className="flip-controls">
         <button
           className={`next-page ${currentPage <= 0 && "button-disabled"}`}
@@ -113,7 +112,7 @@ const Book = () => {
           <MdNavigateNext />
         </button>
       </div>
-    </div>
+    </>
   );
 };
 
